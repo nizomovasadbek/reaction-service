86a09633970e01702b43d9090617f74e43e73c25
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-10-24 22:18:07 +0500

    Just for fun

--- src/main/java/com/test/reaction/ReactionServiceApplication.java
+++ src/main/java/com/test/reaction/ReactionServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 public class ReactionServiceApplication {
-
+	@SuppressWarnings("unchecked")
     public static void main(String[] args) {
         SpringApplication.run(ReactionServiceApplication.class, args);
     }
--- src/main/java/com/test/reaction/dto/GiphyType.java
+++ src/main/java/com/test/reaction/dto/GiphyType.java
@@ -1,8 +1,10 @@
 package com.test.reaction.dto;
 
+import lombok.Setter;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Setter
 @Getter
 @RequiredArgsConstructor
 public enum GiphyType {
